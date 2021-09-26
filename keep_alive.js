ef1de98e51581ae3de2ec50a920803eed671508a
robinn <74510591+itzrobinn@users.noreply.github.com>
2021-09-26 19:26:16 +0200

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 2323;
-app.get('/', (req, res) => res.send('Harmony is Alive!'));
+app.get('/', (req, res) => res.send('HustleMusic is Alive!'));
 
-app.listen(port, () => console.log(`Harmony is listening to http://localhost:${port}`));
+app.listen(port, () => console.log(`HustleMusic is listening to http://localhost:${port}`));
